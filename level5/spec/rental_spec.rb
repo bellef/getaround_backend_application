7f0619af716c37b972e34a55a3abeaf35b7a8359
François Belle <belle.francois.38@gmail.com>
2021-06-06 15:01:04 +0200

    Tests for options

--- level5/spec/rental_spec.rb
+++ level5/spec/rental_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../rental'
 require_relative '../car'
+require_relative '../option'
 
 describe Rental do
   let(:car_1) { Car.new(**{ id: 1, price_per_day: 2000, price_per_km: 10}) }
@@ -17,7 +18,6 @@ describe Rental do
     Rental.new(car, **{ id: 1, car_id: 1, start_date: "2015-07-3", end_date: "2015-07-14", distance: 1000 })
   end
 
-
   describe '#price' do
     context 'with a car costing 20€ a day and 10 cents a km' do
       let(:car) { car_1 }
@@ -27,6 +27,32 @@ describe Rental do
 
         it 'costs 30€' do
           expect(rental.price).to eq(3000)
+        end
+
+        describe '[OPTIONS]' do
+          context 'with a gps' do
+            let(:gps_option) { Option.new(**{ id: 1, rental_id: rental.id, type: 'gps' }) }
+
+            it 'costs 35€' do
+              expect(rental.price).to eq(3500)
+            end
+          end
+
+          context 'with a baby seat' do
+            let(:baby_seat_option) { Option.new(**{ id: 2, rental_id: rental.id, type: 'baby_seat' }) }
+
+            it 'costs 32€' do
+              expect(rental.price).to eq(3200)
+            end
+          end
+
+          context 'with an additional insurance' do
+            let(:additional_insurance_option) { Option.new(**{ id: 3, rental_id: rental.id, type: 'additional_insurance' }) }
+
+            it 'costs 40€' do
+              expect(rental.price).to eq(4000)
+            end
+          end
         end
       end
 
@@ -135,6 +161,35 @@ describe Rental do
               }
             ]
           )
+        end
+
+        describe '[OPTIONS]' do
+          context 'with a gps' do
+            let(:gps_option) { Option.new(**{ id: 1, rental_id: rental.id, type: 'gps' }) }
+
+            it 'gives the extra money to the owner' do
+              owner_action = rental.actions.find { |a| a['who'] == 'owner' }
+              expect(owner_action['amount']).to eq(2600)
+            end
+          end
+
+          context 'with a baby seat' do
+            let(:baby_seat_option) { Option.new(**{ id: 2, rental_id: rental.id, type: 'baby_seat' }) }
+
+            it 'gives the extra money to the owner' do
+              owner_action = rental.actions.find { |a| a['who'] == 'owner' }
+              expect(owner_action['amount']).to eq(2300)
+            end
+          end
+
+          context 'with an additional insurance' do
+            let(:additional_insurance_option) { Option.new(**{ id: 3, rental_id: rental.id, type: 'additional_insurance' }) }
+
+            it 'gives the extra money to drivy' do
+              drivy_action = rental.actions.find { |a| a['who'] == 'drivy' }
+              expect(drivy_action['amount']).to eq(1350)
+            end
+          end
         end
       end
 
